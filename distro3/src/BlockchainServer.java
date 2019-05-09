61d080e6683c776454d2e248e23119291d057a65
Charlie Dunstan <35953283+cwdunstan@users.noreply.github.com>
2019-05-10 07:46:15 +1000

    skeleton code import

---
+++ distro3/src/BlockchainServer.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.HashMap;
+
+public class BlockchainServer {
+
+    public static void main(String[] args) {
+
+        if (args.length != 3) {
+            return;
+        }
+
+        int localPort = 0;
+        int remotePort = 0;
+        String remoteHost = null;
+
+        try {
+            localPort = Integer.parseInt(args[0]);
+            remoteHost = args[1];
+            remotePort = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            return;
+        }
+
+        Blockchain blockchain = new Blockchain();
+
+        HashMap<ServerInfo, Date> serverStatus = new HashMap<ServerInfo, Date>();
+        serverStatus.put(new ServerInfo(remoteHost, remotePort), new Date());
+
+        PeriodicCommitRunnable pcr = new PeriodicCommitRunnable(blockchain);
+        Thread pct = new Thread(pcr);
+        pct.start();
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(localPortNumber);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Thread(new BlockchainServerRunnable(clientSocket, blockchain, serverStatus)).start();
+            }
+        } catch (IllegalArgumentException e) {
+        } catch (IOException e) {
+        } finally {
+            try {
+                pcr.setRunning(false);
+                pct.join();
+                if (serverSocket != null)
+                    serverSocket.close();
+            } catch (IOException e) {
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
